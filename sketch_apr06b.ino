30d381b6051b1c5fa4d70ea003bbc562a85d0ceb
shijingyu <shijy675497282@163.com>
2017-04-08 21:33:28 +0800

    Add files via upload

---
+++ sketch_apr06b.ino
@@ -0,0 +1,20 @@
+char val;
+int ledpin = 13;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(ledpin, OUTPUT);
+}
+
+void loop()
+{
+  val = Serial.read();
+  if (val == 'o')
+  {
+    digitalWrite(ledpin, HIGH);
+    Serial.println("LED ON!");
+  } else if (val == 'f') {
+    digitalWrite(ledpin, LOW);
+    Serial.println("LED OFF!");
+  }
+}
